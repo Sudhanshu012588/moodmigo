d8615690ac0d87522116459b6d2f067d01ecc8df
Sudhanshu012588 <sudhanshunha001@gmail.com>
2025-05-12 12:50:14 +0530

    cahnge

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,9 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-
-      
-        {/* Redirect to dashboard if already logged in */}
-        <Route 
-          path="/login" 
-          element={token ? <Navigate to="/dashboard" /> : <Login />} 
-        />
-        <Route 
-          path="/signup" 
-          element={token ? <Navigate to="/dashboard" /> : <Signup />} 
-        />
-        {token?<Route path="/dashboard" element={<Dashboard />} />:null}
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
   );
