27397138b0c338ee1576968c05cdf45598237457
Sudhanshu012588 <sudhanshunha001@gmail.com>
2025-05-13 20:55:39 +0530

    word limit

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -127,7 +127,7 @@ export default function BlogSection() {
                   name="content"
                   value={blogInput.content}
                   onChange={handleChange}
-                  placeholder="Write your blog content here..."
+                  placeholder="Write your blog content here...!! Limit 1000 words"
                   rows={6}
                   className="w-full border px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
                   required
